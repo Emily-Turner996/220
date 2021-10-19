31155b5e54d068fed6136df56294c5a9c8f5c486
egt817 <egt817@gmail.com>
2021-10-18 21:41:34 -0400

    hw 2

---
+++ assignments/hw7/weighted_average.py
@@ -0,0 +1,17 @@
+"""
+    Name: Emily Turner
+    weighted_average.py
+
+    Problem:
+
+    Certification of Authenticity:
+    I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    # code here
+
+
+if __name__ == '__main__':
+    main()
